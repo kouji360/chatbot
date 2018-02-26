e795d2575a785d576dbbf7a57ade25206af1e80a
kouji sugimoto <kouji36000@gmail.com>
2018-02-27 00:13:42 +0900

    とりあえず画面表示

--- EDU_PRO/js/a-main.js
+++ EDU_PRO/js/a-main.js
@@ -6,7 +6,31 @@ $("#inputArea").change(function () {
         alert("AAA");
     }
    });
+    $("#sendButton").click(function () {
+        var val = $("#inputArea").val();
+        alert(returnAnyKeyword(val));
+    });
 
-//オウム返し機能
+    //オウム返し機能
+    function returnOwm (tag) {
+        return tag+"　ですね！";
+    };
+
+
+    //特定のキーワードに対して、特定の文字列を返す
+    function returnAnyKeyword(keyStr){
+        var tagMaps = new Map();
+        tagMaps.set('ジオン','ジークジオン！');
+        tagMaps.set('おはよう','おはようございます！');
+        tagMaps.set('こんにちは','こんにちは！');
+        tagMaps.set('こんばんわ','こんばんわ！');
+        var rtn = tagMaps.get(keyStr);
+        if(rtn == null){
+            return "「"+keyStr+ "」って、どういう意味ですか？";
+        }else{
+            return rtn;
+        }
+    };
+
 
 });
