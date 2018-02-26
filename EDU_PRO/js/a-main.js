d0f953d3b9785c98c25af7fbaf47e45f048d696f
kouji sugimoto <kouji36000@gmail.com>
2018-02-27 01:56:46 +0900

    とりあえず動くものを

--- EDU_PRO/js/a-main.js
+++ EDU_PRO/js/a-main.js
@@ -1,36 +1,68 @@
-$(function(){
+$(function() {
     //画像出したい
     //内容によって画像変える
-
+    // 下記の情報を持つオブジェクトを定義する
+    // 1.キーワードのリスト
+    // 2.返答内容
+    // 3.画像の種類
 
     //リスナー
     $("#sendButton").click(function () {
         var val = $("#inputArea").val();
         alert(returnAnyKeyword(val));
     });
+    class responseObj {
+        constructor(keywordList, responseMsg, imgSrc) {
+            this.keywordList = keywordList;
+            this.responseMsg = responseMsg;
+            this.imgSrc = imgSrc;
+        }
+    }
 
 
     //ロジック
 
     //オウム返し機能
-    function returnOwm (tag) {
-        return tag+"　ですね！";
+    function returnOwm(tag) {
+        return tag + "　ですね！";
+    };
+
+
+    function getResponseData() {
+        //ここでjson読み込んで、レスポンスオブジェクトのリストを生成する
+        var responseList = [];
+        //とりあえず直打ち
+        responseList.push(new responseObj(["ネオジオン", "シャア"], "ジークジオン！", "../image/笑顔５.png"));
+        responseList.push(new responseObj(["空腹", "お腹すいた", "ごはん"], "どっか食べにいってきなよー", "../image/困り顔１.png"));
+        responseList.push(new responseObj(["こんにちわ"], "こんにちわ", "../image/笑顔２.png"));
+
+        return responseList;
+
     };
 
 
     //特定のキーワードに対して、特定の文字列を返す
-    function returnAnyKeyword(keyStr){
-        var tagMaps = new Map();
-        tagMaps.set('ジオン','ジークジオン！');
-        tagMaps.set('おはよう','おはようございます！');
-        tagMaps.set('こんにちは','こんにちは！');
-        tagMaps.set('こんばんわ','こんばんわ！');
-        var rtn = tagMaps.get(keyStr);
-        if(rtn == null){
-            return "「"+keyStr+ "」って、どういう意味ですか？";
-        }else{
-            return rtn;
+    function returnAnyKeyword(keyStr) {
+        // var tagMaps = new Map();
+        // tagMaps.set('ジオン','ジークジオン！');
+        // tagMaps.set('おはよう','おはようございます！');
+        // tagMaps.set('こんにちは','こんにちは！');
+        // tagMaps.set('こんばんわ','こんばんわ！');
+        // var rtn = tagMaps.get(keyStr);
+
+        const responseList = getResponseData();
+
+        for (let v of responseList) {
+            const keywords = v.keywordList;
+            //キーワードに一致
+            if (keywords.indexOf(keyStr) >= 0) {
+                //画像切り替え
+                $("#yukari").attr('src',v.imgSrc);
+                //キーワードを返す
+                return v.responseMsg;
+            }
         }
+        return "「" + keyStr + "」って、どういう意味ですか？";
     };
 
 
